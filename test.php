28dc06c5782f5b7e99fdf612ceb3c137b953badc
Charles Severance <csev@umich.edu>
2023-04-02 11:06:03 -0400

    Updates

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 
+if( ! defined('STDIN') ) die('Command line only');
+
 require_once "MySpider.php";
 
 $maxpages = 2;
@@ -7,12 +9,12 @@ $maxpages = 2;
 $spider = new MySpider();
 
 // Seed the queue with the starting URL
-$spider->start = "https://online.dr-chuck.com/";
+$spider->start = "http://localhost:8888/localsearchphp/test";
 $spider->first_page($spider->start);
 $results = $spider->crawl($maxpages);
 echo(json_encode($results, JSON_PRETTY_PRINT));
 echo("\n");
-$results = $spider->search('coursera', 0, 10);
+$results = $spider->search('first second', 0, 10);
 echo(json_encode($results, JSON_PRETTY_PRINT));
 echo("\n");
 // $spider->dump(); echo("\n");
