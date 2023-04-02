4914df7f642e4d4a6cbc3d853b978e470018e4c2
Charles Severance <csev@umich.edu>
2023-04-02 11:02:55 -0400

    Add the URLs for crawling and searching.

--- search.php
+++ search.php
@@ -4,14 +4,12 @@ if(is_file("config.php")) { include "config.php"; } else { include "config-dist.
 
 require_once "MySpider.php";
 
-$maxpages = 2;
-
 header('Content-Type: application/json; charset=utf-8');
 
 $spider = new MySpider();
 $spider->start = $spider_start;
 $spider->first_page($spider->start);
-$results = $spider->crawl($maxpages);
+$results = $spider->search($_GET['query'], 0, 10);
 echo(json_encode($results, JSON_PRETTY_PRINT));
 
 
--- test.php
+++ test.php
@@ -7,12 +7,12 @@ $maxpages = 2;
 $spider = new MySpider();
 
 // Seed the queue with the starting URL
-$spider->start = "http://localhost:8888/localsearchphp/test/";
+$spider->start = "https://online.dr-chuck.com/";
 $spider->first_page($spider->start);
 $results = $spider->crawl($maxpages);
 echo(json_encode($results, JSON_PRETTY_PRINT));
 echo("\n");
-$results = $spider->search('first second', 0, 10);
+$results = $spider->search('coursera', 0, 10);
 echo(json_encode($results, JSON_PRETTY_PRINT));
 echo("\n");
 // $spider->dump(); echo("\n");
